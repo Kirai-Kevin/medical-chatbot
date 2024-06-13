6d5e0bddfad4e88db87ef0a849a06fac14257b1c
Kirai-Kevin <kiraikevin627@gmail.com>
2024-06-13 13:06:48 +0300

    setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Medical Chatbot',
+    name = 'Medical-Chatbot',
     version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Kirai Kevin',
+    author_email= 'kiraikevin627@gmail.com',
     packages= find_packages(),
     install_requires = []
 
